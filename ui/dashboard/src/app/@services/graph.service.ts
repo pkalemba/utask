74ac9f9914400e4bba62f977775ac43972935ca4
Simon Martinez <1263852+simonmartinez@users.noreply.github.com>
2020-03-18 12:10:40 +0100

    fix(ui): update dependencies
    Signed-off-by: Simon Martinez <1263852+simonmartinez@users.noreply.github.com>

--- ui/dashboard/src/app/@services/graph.service.ts
+++ ui/dashboard/src/app/@services/graph.service.ts
@@ -52,6 +52,8 @@ export class GraphService {
             .each(function () {
                 tippy(this, {
                     content: $(this).attr('title'),
+                    allowHTML: true,
+                    animation: 'scale'
                 });
             });
         return innerSVG;
